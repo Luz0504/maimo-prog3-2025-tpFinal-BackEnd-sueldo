4f696842b2fbeeee90463fb66d60e78ac7be69e2
Luz <mlsueldo59@gmail.com>
2025-11-13 20:44:27 -0300

    editado y añadido router para users

--- models/user.js
+++ models/user.js
@@ -2,10 +2,8 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
   avatar_url: { type: String },
   joined_at: { type: Date, default: Date.now },
-  role: { type: String, default: "user" },
 });
 
 export default mongoose.model("User", UserSchema); 
---
+++ routes/users.js
@@ -0,0 +1,42 @@
+import express from "express";
+import User from "../models/user.js";
+
+const router = express.Router();
+
+//Crea usuario
+router.post("/", async (req, res) => {
+  try {
+    const { username, avatar_url } = req.body;
+
+    const user = new User({ username, avatar_url });
+    await user.save();
+
+    return res.status(201).send({
+      message: "Usuario creado correctamente",
+      user,
+    });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .send({ message: "Hubo un error al crear el usuario", error });
+  }
+});
+
+//Lista usuarios
+router.get("/", async (_req, res) => {
+  try {
+    const users = await User.find().select("_id username avatar_url");
+    return res.status(200).send({
+      message: "Todos los usuarios",
+      users,
+    });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .send({ message: "Hubo un error al listar los usuarios", error });
+  }
+});
+
+export default router;
